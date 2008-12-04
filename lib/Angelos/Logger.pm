1343d89bc3dc1d39f85989905500d9e21468c2e9
Dann <techmemo@gmail.com>
2008-12-04 15:10:30 +0900

    removed Logger factory

--- lib/Angelos/Logger.pm
+++ lib/Angelos/Logger.pm
@@ -1,18 +1,36 @@
 package Angelos::Logger;
 use Moose;
+use Log::Dispatch::Config;
+use Log::Dispatch::Configurator::YAML;
+use Angelos::Home;
 
 has 'path' => (
-    is => 'rw',
+    is      => 'rw',
     default => sub {
         my $self = shift;
-        Angelos::Home->path_to('log', $self->mode . ".log");
+        Angelos::Home->path_to( 'log', $self->mode . ".log" );
     },
 );
 
+has 'conf_path' => (
+    is      => 'rw',
+    default => sub {
+        Angelos::Home->path_to( 'conf', 'log.yaml' );
+    },
+
+);
+
 has 'mode' => (
-    is => 'rw',
+    is      => 'rw',
     default => sub {
         'debug';
+    }
+);
+
+has 'logger' => (
+    is      => 'rw',
+    default => sub {
+        shift->create;
     }
 );
 
@@ -23,6 +41,13 @@ sub log {
     my $logger = Angelos::Logger::Factory->create;
     $level = $level || 'debug';
     eval { $logger->$level($message); };
+}
+
+sub create {
+    my $self = shift;
+    my $config = Log::Dispatch::Configurator::YAML->new( $self->conf_path );
+    Log::Dispatch::Config->configure($config);
+    Log::Dispatch::Config->instance();
 }
 
 __PACKAGE__->meta->make_immutable;
