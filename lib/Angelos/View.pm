8404278cd5970253a89cc8bee20eadac3063f6ff
Dann <techmemo@gmail.com>
2008-12-04 15:25:35 +0900

    view

--- lib/Angelos/View.pm
+++ lib/Angelos/View.pm
@@ -1,20 +1,15 @@
 package Angelos::View;
 use Moose;
-use Angelos::View::Engine::TT;
 use Angelos::Home;
 
 with 'Angelos::Component';
 
 has 'engine' => (
-    isa     => 'Angelos::View::Engine',
-    default => sub {
-        Angelos::View::Engine::TT->new;
-    }
+    is  => 'rw',
+    isa => 'Angelos::View::Engine',
 );
 
-has 'types' => (
-    is      => 'rw',
-);
+has 'types' => ( is => 'rw', );
 
 has 'format' => (
     is      => 'rw',
@@ -34,14 +29,27 @@ no Moose;
 
 sub render {
     my ( $self, $c, $args ) = @_;
-    my $format        = $c->stash->{format};
-    my $template      = $c->stash->{template};
+    my $format   = $c->stash->{format};
+    my $template = $c->stash->{template};
+
+    #FIXME
     my $template_path = $c->stash->{template_path};
     return undef unless $format || $template || $template_path;
 
     my $stash = { %{ $c->stash } };
     $c->stash->{C} = $self->context;
     $c->stash->{base} ||= $c->req->base;
+
+    my $output = $self->do_render( $c, $template, $stash, $args );
+    return undef unless $output;
+
+    $self->_build_response( $c, $output );
+
+    return 1;
+}
+
+sub _do_render {
+    my ( $self, $c, $template, $stash, $args ) = @_;
     my $output = eval {
         $self->engine->render_template(
             template => $template,
@@ -53,25 +61,22 @@ sub render {
         my $error = "Couldn't render template '$template': $@";
         $c->log( error => $error );
     }
-    return undef unless $output;
 
-    $self->_build_response( $c, $output );
-
-    # Success!
-    return 1;
 }
 
 sub _build_response {
     my ( $self, $c, $output ) = @_;
 
-    # Response
-    my $res = $c->res;
-    $res->code(200) unless $c->res->code;
-    $res->body($output);
+    $c->res->code(200) unless $c->res->code;
+    $c->res->body($output);
+    my $type = $self->types->type( $c->stash->{format} ) || 'text/plain';
 
-    my $type = $self->types->type($c->stash->format) || 'text/plain';
-    $res->headers->content_type($type);
-
+    unless ( $c->res->content_type ) {
+        my $ct      = $self->type;
+        my $charset = 'utf-8';
+        $c->response->content_type("$ct; charset=$charset");
+    }
+    $c->res;
 }
 
 __PACKAGE__->meta->make_immutable;
