3de64df864ca33a36d507802507ca401e40e86de
Dann <techmemo@gmail.com>
2008-12-12 22:58:27 +0900

    added TODO

--- t/lib/TestApp/Web/Controller/Root.pm
+++ t/lib/TestApp/Web/Controller/Root.pm
@@ -6,6 +6,12 @@ extends 'Angelos::Controller';
 sub index {
     my ($self, $c, $params) = @_;
     $c->stash->{template} = 'index';
+    $c->res->body('hello world');
+}
+
+sub view {
+    my ($self, $c, $params) = @_;
+    $c->stash->{template} = 'index';
     $c->view('TT')->render(+{});
 }
 
