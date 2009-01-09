ed7ae920661aaad0ec49f23b6f51b573b49ce9d6
Dann <techmemo@gmail.com>
2009-01-09 22:46:32 +0900

    implemented list manual option

--- lib/Angelos/Script/Command/Manual.pm
+++ lib/Angelos/Script/Command/Manual.pm
@@ -4,7 +4,6 @@ use Angelos::Exceptions;
 use Pod::Simple::Text;
 use Path::Class;
 use utf8;
-binmode STDIN,  ":utf8";
 binmode STDOUT, ":utf8";
 
 =head1 NAME
@@ -14,6 +13,7 @@ Angelos::Script::Command::Manual -
 =head1 DESCRIPTION
 
     % angelos manual --name Tutorial [--lang ja] 
+    % angelos manual --list [--lang ja]
 
 =head1 METHODS
 
@@ -22,25 +22,75 @@ Angelos::Script::Command::Manual -
 =cut
 
 sub opt_spec {
-    return ( [ "topic=s", "manual topic" ], [ "lang=s", "language" ], );
+    return (
+        [ "topic=s", "manual topic" ],
+        [ "lang=s",  "language" ],
+        [ "list",    "list manual topics" ]
+    );
 }
 
 sub validate_args {
     my ( $self, $opt, $arg ) = @_;
 
-    my $topic = $opt->{topic};
+    return if $opt->{list};
 
+    my $topic = $opt->{topic};
     die "You need to give manual topic name with name option\n"
         unless $topic;
 }
 
 sub run {
     my ( $self, $opt, $arg ) = @_;
-    my $topic = $opt->{topic} || 'Tutorial';
-    my $lang  = $opt->{lang}  || 'en';
 
+    my $lang = $opt->{lang} || 'en';
+    if ( $opt->{list} ) {
+        $self->list_manual($lang);
+    }
+    else {
+        my $topic = $opt->{topic} || 'Tutorial';
+        $self->show_manual( $topic, $lang );
+    }
+}
+
+sub list_manual {
+    my ( $self, $lang ) = @_;
+    my $dir = $self->_find_manual_dir($lang);
+    foreach my $file ( $dir->children ) {
+        my $tutorial_name = $file->basename;
+        $tutorial_name =~ s/\.pod$//;
+        print $tutorial_name . "\n";
+    }
+}
+
+sub _find_manual_dir {
+    my ( $self, $lang ) = @_;
+    if ( !$inc ) {
+        my $pkg = __PACKAGE__;
+        $pkg =~ s{::}{/};
+        $inc = substr( __FILE__, 0, -length("$pkg.pm") );
+
+        my $base = $self->help_base;
+        @prefix = ($base);
+        $prefix[0] =~ s{::}{/}g;
+        $base      =~ s{::}{/}g;
+        push @prefix, $base if $prefix[0] ne $base;
+    }
+
+    foreach my $dir ( $inc, @INC ) {
+        foreach my $prefix (@prefix) {
+            foreach my $basename ( ucfirst( lc($topic) ), uc($topic) ) {
+                my $dir = dir( $dir, $prefix, $lang );
+                return $dir if -d $dir;
+            }
+        }
+    }
+}
+
+sub show_manual {
+    my ( $self, $topic, $lang ) = @_;
     if ( my $file = $self->_find_topic( $topic, $lang ) ) {
         my $fh = $file->openr;
+
         #open my $fh, '<:utf8', $file or die $!;
         my $parser = Pod::Simple::Text->new;
         my $buf;
@@ -53,7 +103,6 @@ sub run {
     else {
         die "Cannot find help topic $topic";
     }
-    return;
 }
 
 sub help_base {
@@ -81,8 +130,7 @@ sub _find_topic {
         foreach my $prefix (@prefix) {
             foreach my $basename ( ucfirst( lc($topic) ), uc($topic) ) {
                 foreach my $ext ( 'pod', 'pm' ) {
-                    #my $file = file($dir/$prefix/$lang/$basename.$ext);
-                    my $file = file($dir, $prefix, $lang, "$basename.$ext");
+                    my $file = file( $dir, $prefix, $lang, "$basename.$ext" );
                     return $file if -f $file;
                 }
             }
