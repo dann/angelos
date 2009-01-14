8acc8a5cd16634c235fe34016d1a396b90a3980c
Dann <techmemo@gmail.com>
2009-01-15 00:04:29 +0900

    moved extension classes from core to another package

---
+++ angelos-ext/Angelos-JobQueue/lib/Angelos/JobQueue.pm
@@ -0,0 +1,33 @@
+package Angelos::JobQueue;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Angelos::JobQueue -
+
+=head1 SYNOPSIS
+
+  use Angelos::JobQueue;
+
+=head1 DESCRIPTION
+
+Angelos::JobQueue is
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
