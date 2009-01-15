c96f3303a53d79e17339611b5c27825916692cd2
Dann <techmemo@gmail.com>
2009-01-16 01:39:17 +0900

    now console is pluggable

--- angelos-core/lib/Angelos/Script/Command/Console.pm
+++ angelos-core/lib/Angelos/Script/Command/Console.pm
@@ -1,7 +1,29 @@
 package Angelos::Script::Command::Console;
-use strict;
-use warnings;
+use Mouse;
 use base qw(Angelos::Script::Command);
+use Term::ReadLine;
+with 'Angelos::Class::Pluggable';
+
+has 'term' => (
+    is       => 'rw',
+    required => 1,
+    default  => sub { Term::ReadLine->new('Angelos') }
+);
+
+has 'prompt' => (
+    is       => 'rw',
+    required => 1,
+    default  => sub {'angelos> '}
+);
+
+has 'out_fh' => (
+    is       => 'rw',
+    required => 1,
+    lazy     => 1,
+    default  => sub { shift->term->OUT || \*STDOUT; }
+);
+
+no Mouse;
 
 =head1 NAME
 
@@ -32,19 +54,139 @@ Creates a new console process.
 
 sub run {
     my $self = shift;
+    $self->show_banner;
+    $self->run_loop;
+}
 
-    my $term = new Term::ReadLine 'Angelos Console';
-    my $OUT = $term->OUT || \*STDOUT;
-    my $cxt = Devel::EvalContext->new;
-    while (defined($_ = $term->readline("angelos> "))) {
-        if (/\S/) {
-            my $res = $cxt->run($_);
-            warn $@ if $@;
-            print $OUT $res, "\n" unless $@ || !defined($res);
-            $term->addhistory($_);
-        }
+sub run_loop {
+    my ($self) = @_;
+    while ( $self->run_once_safely ) {
+
+        # keep looping
     }
 }
+
+sub run_once_safely {
+    my ( $self, @args ) = @_;
+
+    my $ret = eval { $self->run_once(@args) };
+
+    if ($@) {
+        my $error = $@;
+        eval { $self->print("Error! - $error\n"); };
+        return 1;
+    }
+    else {
+        return $ret;
+    }
+}
+
+sub run_once {
+    my ($self) = @_;
+
+    my $line = $self->read;
+    return unless defined($line);    # undefined value == EOF
+
+    my @ret = $self->formatted_eval($line);
+
+    $self->print(@ret);
+
+    return 1;
+}
+
+sub formatted_eval {
+    my ( $self, @args ) = @_;
+
+    my @ret = $self->eval(@args);
+
+    return $self->format(@ret);
+}
+
+sub format {
+    my ( $self, @stuff ) = @_;
+
+    if ( $self->is_error( $stuff[0] ) ) {
+        return $self->format_error(@stuff);
+    }
+    else {
+        return $self->format_result(@stuff);
+    }
+}
+
+sub format_result {
+    my ( $self, @stuff ) = @_;
+
+    return @stuff;
+}
+
+sub format_error {
+    my ( $self, $error ) = @_;
+    return $error->stringify;
+}
+
+sub read {
+    my ($self) = @_;
+    return $self->term->readline( $self->prompt );
+}
+
+sub eval {
+    my ( $self, $line ) = @_;
+    my $compiled = $self->compile($line);
+    return $compiled
+        unless defined($compiled);
+    return $self->execute($compiled);
+}
+
+sub compile {
+    my ( $self, @args ) = @_;
+    my $compiled = eval $self->wrap_as_sub(@args);
+    return $self->error_return( "Compile error", $@ ) if $@;
+    return $compiled;
+}
+
+sub wrap_as_sub {
+    my ( $self, $line, %args ) = @_;
+    return qq!sub {\n!
+        . ( $args{no_mangling} ? $line : $self->mangle_line($line) )
+        . qq!\n}\n!;
+}
+
+sub mangle_line {
+    my ( $self, $line ) = @_;
+    return $line;
+}
+
+sub execute {
+    my ( $self, $to_exec, @args ) = @_;
+    my @ret = eval { $to_exec->(@args) };
+    return $self->error_return( "Runtime error", $@ ) if $@;
+    return @ret;
+}
+
+sub is_error {
+    # FIXME
+    return 0;
+}
+
+sub error_return {
+    my ( $self, $type, $error ) = @_;
+}
+
+sub print {
+    my ( $self, @ret ) = @_;
+    my $fh = $self->out_fh;
+    no warnings 'uninitialized';
+    print $fh "@ret";
+    print $fh "\n" if $self->term->ReadLine =~ /Gnu/;
+}
+
+
+sub show_banner {
+    print "Welcome to Angelos Console  (c) 2008 Dann.\n";
+    print "\n" ;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
