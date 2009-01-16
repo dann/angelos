9fb06fcc0661e3591a77662c96db184af1bae31b
Dann <techmemo@gmail.com>
2009-01-16 17:34:21 +0900

    added TODO note

--- angelos-core/lib/Angelos/View/TT.pm
+++ angelos-core/lib/Angelos/View/TT.pm
@@ -5,9 +5,34 @@ use Angelos::Home;
 use Path::Class;
 extends 'Angelos::View';
 
-has 'INCLUDE_PATH' => ( is => 'rw', );
+has 'INCLUDE_PATH'       => ( is       => 'rw', );
 has 'TEMPLATE_EXTENSION' => ( +default => '.tt' );
 has 'CONTENT_TYPE'       => ( +default => 'text/html' );
+
+# TODO: Implement at _build_engine
+# _build_providers
+has 'PROVIDERS' => (
+    is  => 'rw',
+    isa => 'ArrayRef',
+);
+
+# TODO: Implement at _build_engine
+has 'PRE_PROCESS' => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+# TODO: Implement at _build_engine
+has 'WRAPPER' => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+# TODO: Implement at _build_engine
+has 'TIMER' => (
+    is  => 'rw',
+    isa => 'Str',
+);
 
 no Mouse;
 
@@ -15,7 +40,13 @@ sub _build_engine {
     my $self = shift;
     my $include_path ||= $self->INCLUDE_PATH;
     $include_path    ||= $self->root;
-    Template->new( INCLUDE_PATH => $include_path );
+
+    my $config = {
+        EVAL_PERL    => 0,
+        INCLUDE_PATH => $include_path,
+    };
+
+    Template->new(%$config);
 }
 
 sub _render {
