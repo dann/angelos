d4418c9c538a35088e234aae135798db536f51a3
Dann <techmemo@gmail.com>
2009-01-18 00:18:55 +0900

    execute module-setup script directly

--- angelos-core/lib/Angelos/Script/Command/Generate.pm
+++ angelos-core/lib/Angelos/Script/Command/Generate.pm
@@ -32,7 +32,7 @@ sub run {
 
     my $flavor = $opt->{flavor} || 'app';
     my $module = $opt->{name};
-
+    
     $self->generate( $flavor, $module );
 }
 
@@ -47,23 +47,13 @@ sub validate_args {
 
 sub generate {
     my ( $self, $flavor, $module ) = @_;
-    local $ENV{MODULE_SETUP_DIR} = '/tmp/module-setup';
-    my $plugins      = [];
     my $flavor_class = $self->to_flavor_class($flavor);
-    my $options      = {
-        module       => $module,
-        plugins      => $plugins,
-        flavor_class => $flavor_class,
-        direct       => 1,
-    };
-    my $argv = [$module];
-    my $pmsetup = Module::Setup->new( options => $options, argv => $argv );
-    $pmsetup->run( $options, [ $module, $flavor_class ] );
+    $self->system('module-setup', '--direct', '--flavor-class=+' . $flavor_class, $module); 
 }
 
 sub to_flavor_class {
     my ( $self, $flavor_type ) = @_;
-    "+Angelos::Script::Command::Generate::Flavor::" . camelize($flavor_type);
+    "Angelos::Script::Command::Generate::Flavor::" . camelize($flavor_type);
 }
 
 1;
