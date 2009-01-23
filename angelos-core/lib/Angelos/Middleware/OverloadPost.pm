0ac0210bf1b6ca390d3218295fd3d57ed214d23c
Dann <techmemo@gmail.com>
2009-01-23 21:46:08 +0900

    added Overload Post middleware

---
+++ angelos-core/lib/Angelos/Middleware/OverloadPost.pm
@@ -0,0 +1,26 @@
+package Angelos::Middleware::OverloadPost;
+use Angelos::Class;
+extends 'Angelos::Middleware';
+
+sub wrap {
+    my ( $self, $next ) = @_;
+
+    sub {
+        my $req = shift;
+        $self->overload_request_method($req);
+        my $res = $next->($req);
+        $res;
+    }
+}
+
+sub overload_request_method {
+    my ( $self, $req ) = @_;
+    my $overload = $req->param('_method')
+        || $req->param('x-tunneled-method')
+        || $req->header('X-HTTP-Method-Override');
+    $req->method($overload) if $overload;
+    $req;
+}
+
+__END_OF_CLASS__
+
