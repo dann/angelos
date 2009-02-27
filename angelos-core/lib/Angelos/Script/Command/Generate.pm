162cef8ea5bc9e54c02c8b79924030f47e21cea9
Dann <techmemo@gmail.com>
2009-02-27 21:56:54 +0900

    make scripts executable

--- angelos-core/lib/Angelos/Script/Command/Generate.pm
+++ angelos-core/lib/Angelos/Script/Command/Generate.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use base qw(Angelos::Script::Command);
 use Module::Setup;
+use Path::Class;
+use Carp ();
 use String::CamelCase qw(camelize);
 use Angelos::Exceptions;
 
@@ -21,10 +23,7 @@ Angelos::Script::Command::Generate - A generator for your Angelos application
 =cut
 
 sub opt_spec {
-    return (
-        [ "flavor=s", "flavor name" ],
-        [ "name=s",   "name" ],
-    );
+    return ( [ "flavor=s", "flavor name" ], [ "name=s", "name" ], );
 }
 
 sub run {
@@ -32,23 +31,37 @@ sub run {
 
     my $flavor = $opt->{flavor} || 'app';
     my $module = $opt->{name};
-    
+
     $self->generate( $flavor, $module );
+
+    my $module_dir = dir( split '::', $module );
+    $self->make_scripts_executable($module);
 }
 
 sub validate_args {
     my ( $self, $opt, $arg ) = @_;
     my $module = $opt->{name};
 
-    Angelos::Exception::ParameterMissingError->throw(
-        message => "You need to give your new module name --name\n")
-        unless $module;
+    Carp::croak "You need to give your new module name --name" unless $module;
 }
 
 sub generate {
     my ( $self, $flavor, $module ) = @_;
     my $flavor_class = $self->to_flavor_class($flavor);
-    $self->system('module-setup', '--direct', '--flavor-class=+' . $flavor_class, $module); 
+    $self->system( 'module-setup', '--direct',
+        '--flavor-class=+' . $flavor_class, $module );
+}
+
+sub make_scripts_executable {
+    my ( $self, $module_dir ) = @_;
+
+    my @scripts = (
+        dir( $module_dir, 'bin' )->children,
+        dir( $module_dir, 'tools' )->children
+    );
+    for my $script (@scripts) {
+        $self->system( 'chmod', '755', $script );
+    }
 }
 
 sub to_flavor_class {
