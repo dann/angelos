7478e2c0d3ea8af048b160cc83fe12b1ad6ec629
Dann <techmemo@gmail.com>
2009-04-14 12:45:21 +0900

    tested

--- angelos-core/tools/test_format_junit.pl
+++ angelos-core/tools/test_format_junit.pl
@@ -4,6 +4,8 @@ use warnings;
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
 
+warn $FindBin::RealBin;
+
 use TAP::Harness::JUnit;
 my $harness = TAP::Harness::JUnit->new(
     {   xmlfile => 'TEST-RESULT.xml',
